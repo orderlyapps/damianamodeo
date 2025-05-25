52c75d81fa5a9ff3f47f24a50647f22848b20e76
orderlyapps <orderlyapps@gmail.com>
2025-05-26 09:01:12 +1000

    Updates

---
+++ assets/buttons-UPUwJmKh.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-DJW6srJ3.js";const e=function(){return n.jsx("div",{children:'Hello "/mantine/buttons"!'})};export{e as component};
---
+++ assets/text-CxqgvusG.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-DJW6srJ3.js";const e=function(){return n.jsx("div",{children:'Hello "/(mantine)/_mantine/text"!'})};export{e as component};
